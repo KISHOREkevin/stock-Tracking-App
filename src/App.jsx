c560240c24378923b1ab7293a15b4d44aa46fec4
Kishore Kevin <kishorekevin3289@gmail.com>
2023-07-14 11:43:20 +0530

    update project

--- src/App.jsx
+++ src/App.jsx
@@ -53,9 +53,6 @@ export default function TradingViewWidget() {
   return (
     <div className='tradingview-widget-container'>
       <div id='tradingview_2904e' />
-      <div className="tradingview-widget-copyright">
-        <a href="https://www.tradingview.com/" rel="noopener nofollow noreferrer" target="_blank"><span className="blue-text">Track all markets on TradingView</span></a>
-      </div>
     </div>
   );
 }
